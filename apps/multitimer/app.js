697cb7325da21af579f1f9042857e130fa34045a
mattcr11 <mattcr11@users.noreply.github.com>
2023-10-17 19:31:57 -0400

    Revert variable removal - caused undefined variable error
    This was removed in an apparent code cleanup, but breaks timer draw updates on banglejs2

--- apps/multitimer/app.js
+++ apps/multitimer/app.js
@@ -78,7 +78,7 @@ function drawTimers() {
     }, 1000 - (timers[idx].t % 1000));
   }
 
-  E.showScroller({
+  var s = E.showScroller({
     h : 40, c : timers.length+2,
     back : function() {load();},
     draw : (idx, r) => {
@@ -138,7 +138,7 @@ function timerMenu(idx) {
     }, 1000 - (a.t % 1000));
   }
 
-  E.showScroller({
+  var s = E.showScroller({
     h : 40, c : 5,
     back : function() {
       clearInt();
@@ -341,7 +341,7 @@ function drawSw() {
     }, 1000 - (sw[idx].t % 1000));
   }
 
-  E.showScroller({
+  var s = E.showScroller({
     h : 40, c : sw.length+2,
     back : function() {load();},
     draw : (idx, r) => {
@@ -420,7 +420,7 @@ function swMenu(idx, a) {
     setUI();
   }
 
-  E.showScroller({
+  var s = E.showScroller({
     h : 40, c : 5,
     back : function() {
       clearInt();
@@ -508,7 +508,7 @@ function drawAlarms() {
   layer = 2;
   const alarms = require("sched").getAlarms().filter(a => !a.timer);
 
-  E.showScroller({
+  var s = E.showScroller({
     h : 40, c : alarms.length+2,
     back : function() {load();},
     draw : (idx, r) => {
